d8171575e9f66350e7fbbfab7b3cc37f5da53aa6
kauas <kauasoussa12345@gmail.com>
2025-12-11 14:47:18 -0300

    pagamento sendo concluido..

--- tasks/poo/pagamento/py/draft.py
+++ tasks/poo/pagamento/py/draft.py
@@ -25,7 +25,8 @@ class CartaoCredito(Pagamento):
             raise Exception(f"Limite insuficiente no cartão")
         else:
             self.limite_disp -= self.valor
-            print(f"Pagamento aprovado")
+            print(f"Pagamento aprovado no cartão {self.nome_titu}.    "
+                  f"Limite restante {self.limite_disp}")
 
 class Pix(Pagamento):
     def __init__(self, valor, descricao, chave, banco):
@@ -34,7 +35,7 @@ class Pix(Pagamento):
         self.banco = banco
 
     def processar(self):
-        print(f"Pix enivado via {self.banco} usando a chave {self.chave}")
+        print(f"Pix enviado via {self.banco} usando a chave {self.chave}")
 
 class Boleto(Pagamento):
     def __init__(self, valor, descricao, codigo_barras, vencimento):
@@ -44,6 +45,20 @@ class Boleto(Pagamento):
 
     def processar(self):
         print(f"Boleto gerado. Aguardando pagamento.....")
+
+class Fiado(Pagamento):
+    def __init__(self, valor, descricao, cliente, limite_fiado):
+        super().__init__(valor, descricao)
+        self.cliente = cliente
+        self.limite_fiado = limite_fiado
+
+    def processar(self):
+        if self.valor > self.limite_fiado:
+            raise Exception(f"Fiado negado para {self.cliente}: limite insuficiente!")
+        self.limite_fiado -= self.valor
+        print(f"Compra no fiado registrada para {self.cliente}.  "
+              f"Limite restante: R$ {self.limite_fiado}"
+              )
 
 def processar_pagamento(pagamento = Pagamento):
     try:
@@ -55,9 +70,11 @@ def processar_pagamento(pagamento = Pagamento):
 
 pagamentos = [
     Pix(150, "Camisa esportiva", "email@ex.com", "Banco XPTO"),
+    Pix(0, "Cafe", "senna@ufc.com", "Banco POO"),
     CartaoCredito(400, "Ténis esportivo", "1234 5678 9123 4567", "Cliente X", 500),
     Boleto(89.90, "Livro de Python", "123456789000", "2025-01-10"),
-    CartaoCredito(800, "Notebook", "9999 8888 7777 6666", "Cliente Y", 700)
+    CartaoCredito(800, "Notebook", "9999 8888 7777 6666", "Cliente Y", 700),
+    Fiado(2999, "Notebook Gamer", "David Senna", 3000)
 ]
 for p in pagamentos:
     processar_pagamento(p)
