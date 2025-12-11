e32b02eccd8539abf004714cc4a38526e4eab340
kauas <kauasoussa12345@gmail.com>
2025-12-11 11:15:55 -0300

    teu pagamento

--- tasks/poo/pagamento/py/draft.py
+++ tasks/poo/pagamento/py/draft.py
@@ -15,7 +15,7 @@ class Pagamento:
 
 class CartaoCredito(Pagamento):
     def __init__(self, valor, descricao, numero, nome_titu, limite_disp):
-        super.__init__(valor, descricao)
+        super().__init__(valor, descricao)
         self.numero = numero
         self.nome_titu = nome_titu
         self.limite_disp = limite_disp
@@ -36,11 +36,14 @@ class Pix(Pagamento):
     def processar(self):
         print(f"Pix enivado via {self.banco} usando a chave {self.chave}")
 
+class Boleto(Pagamento):
+    def __init__(self, valor, descricao, codigo_barras, vencimento):
+        super().__init__(valor, descricao)
+        self.codigo_barras = codigo_barras
+        self.vencimento = vencimento
 
-
-
-
-
+    def processar(self):
+        print(f"Boleto gerado. Aguardando pagamento.....")
 
 def processar_pagamento(pagamento = Pagamento):
     try:
@@ -50,4 +53,12 @@ def processar_pagamento(pagamento = Pagamento):
     except Exception as e:
         print(e)
 
+pagamentos = [
+    Pix(150, "Camisa esportiva", "email@ex.com", "Banco XPTO"),
+    CartaoCredito(400, "Ténis esportivo", "1234 5678 9123 4567", "Cliente X", 500),
+    Boleto(89.90, "Livro de Python", "123456789000", "2025-01-10"),
+    CartaoCredito(800, "Notebook", "9999 8888 7777 6666", "Cliente Y", 700)
+]
+for p in pagamentos:
+    processar_pagamento(p)
 
